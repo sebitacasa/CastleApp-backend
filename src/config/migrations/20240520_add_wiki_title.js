684334c2699df3a81d11629261b422870b4571d0
sebitacasa <seba.-14@hotmail.com>
2026-01-29 08:50:06 -0300

    wiki-title add in db

--- src/config/migrations/20240520_add_wiki_title.js
+++ src/config/migrations/20240520_add_wiki_title.js
@@ -1,13 +1,19 @@
-exports.up = function(knex) {
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
   return knex.schema.table('historical_locations', (table) => {
-    // Definimos la columna como string y permitimos explícitamente que sea nula
-    table.string('wiki_title').nullable(); 
+    table.string('wiki_title').nullable();
   });
-};
+}
 
-exports.down = function(knex) {
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
   return knex.schema.table('historical_locations', (table) => {
-    // En el rollback, eliminamos la columna para volver al estado anterior
     table.dropColumn('wiki_title');
   });
-};
+}
